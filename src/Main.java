ddb4b4cc88c92f07d5b61918896b1bef236547e4
Øystein <ystein@172.16.14.126>
2011-09-24 14:46:19 +0200

    Eg var her og git!

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello asslickers");
 		System.out.println("mr. jones was here");
+		System.out.println("mr. waage was here too biaaatcheesss");
 	}
 }
